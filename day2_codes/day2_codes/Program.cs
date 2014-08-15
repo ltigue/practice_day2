944d5de288f544dc5dcee380f0e3426612e3987a
ltigue <moneynloans@yahoo.com>
2014-08-15 10:42:04 -0400

    change

--- day2_codes/day2_codes/Program.cs
+++ day2_codes/day2_codes/Program.cs
@@ -1,33 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
-namespace day2_codes
+namespace ReflectionTest
 {
     class Program
     {
+        private static int a = 5, b = 10, c = 20;
         static void Main(string[] args)
         {
-           
-            int result = AddNumbers(10, 5);
-            Console.WriteLine(result);
+            Console.WriteLine("a + b + c =" + (a + b + c));
+            Console.WriteLine("Please enter the name of the variable that you wish to change:");
+            string varName = Console.ReadLine();
+            Type t = typeof(Program);
+            FieldInfo fieldInfo = t.GetField(varName, BindingFlags.NonPublic | BindingFlags.Static);
+            if(fieldInfo != null)
+            {
+                Console.WriteLine("The current value of " + fieldInfo.Name + " is" + fieldInfo.GetValue(null) + ".  You may enter a value now:");
+                string newValue = Console.ReadLine();
+                int newInt;
+
+                if( int.TryParse(newValue, out newInt))
+                {
+                    fieldInfo.SetValue(null, newInt);
+                    Console.WriteLine("a + b + c = " + (a + b + c));
+                }
+                Console.ReadKey();
+            }
+         }
+    
+    }
             
            
-        }
-        public static int AddNumbers(int number1, int number2)
-        {
-            int result = number1 + number2;
-            if (result > 10)
-            {
-                return result;
-            }
-            return 0;
-        }
-
-
-        
-    }
-
 }
+
